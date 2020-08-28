7b1b5a96fc4d6dc1a0d6e041b0ab6568e7eebc52
Sliderpro <dmitry.romanov93@gmail.com>
2020-08-28 19:58:29 +0300

    Add files via upload
    A while loop, Math.random, if.

---
+++ Smokingtires.java
@@ -0,0 +1,27 @@
+
+public class Smokingtires {
+	public static void main(String[] args) {
+		//BICT shows some incredible loop, if and random mastery!
+		System.out.println("Engine starts!");
+		int x = 1;
+		double y = Math.random();
+		//Car is showing off!
+		while (y<=1)	 {
+			if (x<2) {
+			System.out.println("Tires are cold");
+			}
+			System.out.println("Wheel goes shhh and makes smoke!");
+			x++;
+			y+=0.1;
+			//Wheel conditions
+			if (x==5) {
+				System.out.println("Tires are hot!");
+			}
+			if (x==9) {
+				System.out.println("Tires are almost exploding!");
+			}
+						 }
+		System.out.println("Engine powers down!");
+		//Tires almost went boom, thanks!
+	}	
+}
